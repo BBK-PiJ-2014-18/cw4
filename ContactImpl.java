57084eae73c110a2348634a42b6886a6b27fde89
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-03-06 14:21:44 +0000

    Pass Test 5 (overides on equals & hashcode)

--- ContactImpl.java
+++ ContactImpl.java
@@ -29,4 +29,27 @@ public class ContactImpl implements Contact {
 	public void addNotes(String note) {
 		// TODO Auto-generated method stub
 	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if(this == other) return true;
+		if(other == null || (this.getClass() != other.getClass())){
+			return false;
+		}
+		Contact contact = (Contact) other;
+		return (this.getId() == contact.getId()
+				&& this.getName() == contact.getName()
+				&& this.getNotes() == contact.getNotes());
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = 0;
+		result = 31 * result + contactId;
+		result = 31 * result + contactName.hashCode();
+		result = 31 * result + (contactNotes != null ? contactNotes.hashCode() : 0);
+		return result;
+	}
+	
+	
 }
